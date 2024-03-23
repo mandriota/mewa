4c46ae662a6425d7c970790f2aba3afbee42df28
Mark Mandriota <myriaxont@gmail.com>
2024-03-23 21:41:26 +0100

    add config file

---
+++ config.h
@@ -0,0 +1,35 @@
+/******************************************************************************\
+*                                                                              *
+*    Mewa. Math EWAluator.                                                     *
+*    Copyright (C) 2024 Mark Mandriota                                         *
+*                                                                              *
+*    This program is free software: you can redistribute it and/or modify      *
+*    it under the terms of the GNU General Public License as published by      *
+*    the Free Software Foundation, either version 3 of the License, or         *
+*    (at your option) any later version.                                       *
+*                                                                              *
+*    This program is distributed in the hope that it will be useful,           *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*    GNU General Public License for more details.                              *
+*                                                                              *
+*    You should have received a copy of the GNU General Public License         *
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.    *
+*                                                                              *
+\******************************************************************************/
+
+//=:config
+//                      __ _
+//                     / _(_)
+//      ___ ___  _ __ | |_ _  __ _
+//     / __/ _ \| '_ \|  _| |/ _` |
+//    | (_| (_) | | | | | | | (_| |
+//     \___\___/|_| |_|_| |_|\__, |
+//                            __/ |
+//                           |___/
+
+// comment to disable debug mode
+// #define NDEBUG
+
+// uncomment to disable colors
+// #define NCOLORS
