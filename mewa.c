d9f7d26fe9120bd6676c55ebcb3479778a8d5a0b
Mark Mandriota <myriaxont@gmail.com>
2024-03-28 23:35:44 +0100

    try fix

--- mewa.c
+++ mewa.c
@@ -695,7 +695,8 @@ enum PR_ERR pr_rl_biop_next_node(struct Parser *pr, struct Node **node,
     DBG_PRINT("%s: setting node\n", __func__);
 	DBG_PRINT("%s: node type: %s\n", __func__, nt_stringify((*node)->type));
 	DBG_PRINT("%s: node.a type: %s\n", __func__, nt_stringify((*node)->as.bp.a->type));
-    **node = *(*node)->as.bp.a; // segfault!
+	struct Node tmp = *(*node)->as.bp.a;
+    **node = tmp; // segfault!
     DBG_PRINT("%s: node set\n", __func__);
   }
 
